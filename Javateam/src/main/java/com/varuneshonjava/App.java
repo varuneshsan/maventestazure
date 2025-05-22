af42276f4a2ac7b51dda851b50565541d9e2a915
varuneshsan <varunesh@cambridge.edu.in>
2025-05-23 05:19:20 +0530

    azure pipeline example

---
+++ Javateam/src/main/java/com/varuneshonjava/App.java
@@ -0,0 +1,13 @@
+package com.varuneshonjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
